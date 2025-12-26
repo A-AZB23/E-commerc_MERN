db3b0a84900402558cf0c0911525c149e4110b00
Abdelrhman Azb <AbdelrhmanAz@gmail.com>
2025-12-26 23:26:57 +0200

    the updated items done

--- src/routes/cartRoutes.ts
+++ src/routes/cartRoutes.ts
@@ -1,7 +1,11 @@
 // src/routes/cartRouter.ts
 
 import express from "express";
-import { addItemToCart, getUserCart } from "../services/cartService";
+import {
+  addItemToCart,
+  getUserCart,
+  updateItemInCart,
+} from "../services/cartService";
 import validateJWT from "../middlewares/validateJWT";
 
 const router = express.Router();
@@ -15,32 +19,43 @@ router.get("/", validateJWT, async (req, res) => {
   try {
     // FIX: Use the authenticated user's ID from the request object
     const cart = await getUserCart({ userId: req.user._id.toString() });
-    res.status(200).send(cart);  } 
-  catch (error) {
+    res.status(200).send(cart);
+  } catch (error) {
     res.status(500).send("Internal Server Error");
   }
 });
 
 interface CartResponse {
-    statusCode: number;
-    data: any;
+  statusCode: number;
+  data: any;
 }
-
 
 router.post("/items", validateJWT, async (req, res) => {
   if (!req.user || !req.user._id) {
     return res.status(401).send("Authentication failure: User ID missing.");
   }
 
-const userId = req.user._id.toString();
-const { productId, quantity } = req.body;
-const response = await addItemToCart({ userId, productId, quantity });
+  const userId = req.user._id.toString();
+  const { productId, quantity } = req.body;
+  const response = await addItemToCart({ userId, productId, quantity });
 
-if (!response) {
-  return res.status(500).send("Internal Server Error");
-}
+  if (!response) {
+    return res.status(500).send("Internal Server Error");
+  }
 
-res.status(response.statusCode).send(response.data);
+  res.status(response.statusCode).send(response.data);
+});
+
+router.put("/items", validateJWT, async (req, res) => {
+  if (!req.user || !req.user._id) {
+    return res.status(401).send("Authentication failure: User ID missing.");
+  }
+
+  const userId = req.user._id.toString();
+  const { productId, quantity } = req.body;
+  const response = await updateItemInCart({ userId, productId, quantity });
+
+  res.status(response.statusCode).send(response.data);
 });
 
 export default router;
--- src/services/cartService.ts
+++ src/services/cartService.ts
@@ -49,18 +49,81 @@ export const addItemToCart = async ({
 
   if (existsInCart) {
     return { data: "item alreadry exists in cart", statusCode: 400 };
+
+
   }
 
+
+//fetch
+
   const products = await productModel.findById(productId);
+
   if (!products) {
-    return;
+    return{ data: "Product not found", statusCode: 404 };
+  }
+
+  if(products.stock < quantity){
+    return{data: "low stock", statusCode: 400};
   }
 
   cart.items.push({ products: productId, unitPrice: products.price, quantity });
-  
+
   cart.totalAmount += products.price * quantity;
   await cart.save();
   return { data: cart, statusCode: 200 };
 };
 
-export default { getUserCart, addItemToCart };
+interface updateItemToCart {
+  userId: string;
+  productId: any;
+  quantity: number;
+}
+
+export const updateItemInCart = async ({
+  userId,
+  productId,
+  quantity,
+}: updateItemToCart) => {
+  const cart = await getUserCart({ userId });
+
+  const existsInCart = cart.items.find(
+    (p) => p.products.toString() === productId
+  );
+
+  if (!existsInCart) {
+    return { data: "Item does not exist in cart", statusCode: 400 };
+  }
+
+
+    const products = await productModel.findById(productId);
+  if (!products) {
+    return{ data: "Product not found", statusCode: 404 };
+  }
+  if(products.stock < quantity){
+    return{data: "low stock", statusCode: 400};
+  }
+
+
+
+  const otherItems = cart.items.filter(
+    (p) => p.products.toString() !== productId
+  );
+  
+  let total = otherItems.reduce((sum, product) => {
+    sum += product.quantity * product.unitPrice;
+    return sum;
+  }, 0);
+  
+  existsInCart.quantity = quantity;
+  total += existsInCart.quantity * existsInCart.unitPrice;  
+  cart.totalAmount = total;
+
+
+  const updatedCart = await cart.save();
+  return { data: updatedCart, statusCode: 200 };
+
+
+
+};
+
+export default { getUserCart, addItemToCart, updateItemInCart };
